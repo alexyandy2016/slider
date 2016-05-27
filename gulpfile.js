66f2d361e949cb9bbf1d6eee81d576b6dd2b258c
Chen Fengyuan <chenfengyuan@outlook.com>
2016-05-27 14:55:18 +0800

    Use Gulp

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var pkg = require('./package');
+var now = new Date();
+var scripts = {
+  all: [
+    'gulpfile.js',
+    'src/slider.js',
+    'docs/js/main.js'
+  ],
+  main: 'dist/slider.js',
+  docs: 'docs/js',
+  site: '_gh_pages/js',
+  src: 'src/slider.js',
+  dest: 'dist'
+};
+var replacement = {
+  regexp: /@\w+/g,
+  filter: function (placeholder) {
+    switch (placeholder) {
+      case '@VERSION':
+        placeholder = pkg.version;
+        break;
+
+      case '@YEAR':
+        placeholder = now.getFullYear();
+        break;
+
+      case '@DATE':
+        placeholder = now.toISOString();
+        break;
+    }
+
+    return placeholder;
+  }
+};
+
+gulp.task('jscopy', function () {
+  return gulp.src(scripts.src)
+  .pipe(gulp.dest(scripts.docs))
+  .pipe(gulp.dest(scripts.dest));
+});
+
+gulp.task('jshint', function () {
+  return gulp.src(scripts.all)
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('default'));
+});
+
+gulp.task('jscs', function () {
+  return gulp.src(scripts.all)
+    .pipe(plugins.jscs())
+    .pipe(plugins.jscs.reporter());
+});
+
+gulp.task('js', ['jshint', 'jscs'], function () {
+  return gulp.src(scripts.main)
+    .pipe(plugins.replace(replacement.regexp, replacement.filter))
+    .pipe(gulp.dest(scripts.dest))
+    .pipe(gulp.dest(scripts.site))
+    .pipe(plugins.rename({
+      suffix: '.min'
+    }))
+    .pipe(plugins.uglify({
+      preserveComments: 'license'
+    }))
+    .pipe(gulp.dest(scripts.dest))
+    .pipe(gulp.dest(scripts.site));
+});
+
+gulp.task('htmlcomb', function () {
+  return gulp.src('docs/*.html')
+    .pipe(plugins.htmlcomb())
+    .pipe(gulp.dest('docs'));
+});
+
+gulp.task('docs', function () {
+  return gulp.src('docs/**')
+    .pipe(gulp.dest('_gh_pages'));
+});
+
+gulp.task('release', ['docs'], function () {
+  return gulp.src('dist/*.{js,css}')
+    .pipe(gulp.dest('_releases/' + pkg.version));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(scripts.src, ['jscopy']);
+  gulp.watch('docs/**', ['docs']);
+});
+
+gulp.task('default', ['watch']);
